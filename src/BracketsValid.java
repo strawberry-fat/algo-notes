bcc9a87cafa509875af16ecac75eb42c4ef40375
lijinxing <lijinx-pccw@139.com>
2020-02-12 20:45:24 +0800

    有效的括号

---
+++ src/BracketsValid.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+/**
+ * 有效的括号
+ * https://leetcode-cn.com/problems/valid-parentheses/
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class BracketsValid {
+    public boolean isValid(String s) {
+        if (s.length() == 0)
+            return true;
+        if ((s.length() & 1) == 1)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(s.charAt(i));
+                    continue;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(')
+                        return false;
+                    continue;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                    continue;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{')
+                        return false;
+                    continue;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
